2edeb7afaead700a9ded5dc7d8a0bf98a2c46844
Louis Nicolle <louisxnicolle@gmail.com>
2025-03-20 13:16:11 +0100

    Fhufhur

--- helpinglouis/enum.py
+++ helpinglouis/enum.py
@@ -3,4 +3,4 @@ from django.db import models
 class NameChoice(models.TextChoices):
     OUI = "oui", "Oui"
     NON = "non", "Non"
-    COUP_DE_COEUR = "coup_de_coeur", "Coup de cœur"
+    COUP_DE_COEUR = "coup_de_coeur", "Coup de fhuhfur"
