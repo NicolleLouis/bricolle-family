fca4623cf91194b0f6d864f1333a0c0ccd49323d
Louis Nicolle <louisxnicolle@gmail.com>
2025-03-30 16:36:02 +0200

    Display front shopping_list

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -6,6 +6,7 @@ from shopping_list.views.list import shopping_list
 from shopping_list.views.login import Login
 from shopping_list.views.meal import meal
 from shopping_list.views.planned_course import PlannedCourseController
+from shopping_list.views.planned_ingredients import PlannedIngredientController
 
 app_name = "shopping_list"
 
@@ -17,8 +18,13 @@ urlpatterns = [
     path("meal/", meal, name="meal"),
     path("register/", Login.register, name="register"),
     path("shopping_list", shopping_list, name="list"),
-    path("planned_courses/", PlannedCourseController.index, name="planned_courses"),
+    path("planned_course/", PlannedCourseController.index, name="planned_course"),
     path("planned_course/add", PlannedCourseController.add_api, name="planned_course_add_api"),
     path("planned_course/delete", PlannedCourseController.delete, name="planned_course_delete"),
     path("planned_course/new", PlannedCourseController.add_page, name="planned_course_add_page"),
+
+    path("shopping_list/", PlannedIngredientController.index, name="shopping_list"),
+    path("shopping_list/add", PlannedIngredientController.add_api, name="shopping_list_add_api"),
+    path("shopping_list/delete", PlannedIngredientController.delete, name="shopping_list_delete"),
+    path("shopping_list/new", PlannedIngredientController.add_page, name="shopping_list_add_page"),
 ]
