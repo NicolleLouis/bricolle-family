b94edf5e7580593cefd2c300938002d50cdf0a64
Louis Nicolle <louisxnicolle@gmail.com>
2025-05-02 22:01:11 +0200

    Change static files directory

--- bricolle_family/settings.py
+++ bricolle_family/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 
 STORAGES["staticfiles"] = {"BACKEND": 'whitenoise.storage.CompressedManifestStaticFilesStorage'}
 
