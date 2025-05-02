ed41d816a60ce1cbb6713c81d4203e413dc393a0
Louis Nicolle <louisxnicolle@gmail.com>
2025-05-02 21:25:30 +0200

    Remove old nginx

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 echo "📦 Pulling latest changes from Git..."
 git pull origin main
 
+echo "🧹 Cleaning old containers..."
+docker compose down -v
+
 echo "🐳 Rebuilding containers..."
 docker compose build
 
