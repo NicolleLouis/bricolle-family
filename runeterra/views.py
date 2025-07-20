bb6b3e29a6612e6a04e5d8a81fdadf8d86902086
NicolleLouis <louisxnicolle@gmail.com>
2025-07-20 13:42:53 +0200

    Add region stats page to Runeterra app

--- runeterra/views.py
+++ runeterra/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Q
+from django.db.models import Avg, Case, F, IntegerField, Q, When
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView
@@ -105,3 +105,23 @@ class ChampionUpdateView(UpdateView):
     form_class = ChampionForm
     template_name = "runeterra/champion_form.html"
     success_url = reverse_lazy("runeterra:champion_list")
+
+
+def region_stats(request):
+    stats = []
+    for value, label in Region.choices:
+        qs = Champion.objects.filter(Q(primary_region=value) | Q(secondary_region=value))
+        total = qs.count()
+        unlocked = qs.filter(unlocked=True).count()
+        avg = (
+            qs.annotate(
+                effective=F("star_level")
+                + Case(When(lvl30=True, then=1), default=0, output_field=IntegerField())
+            )
+            .aggregate(avg=Avg("effective"))
+            .get("avg")
+            or 0
+        )
+        stats.append({"label": label, "total": total, "unlocked": unlocked, "avg": avg})
+
+    return render(request, "runeterra/region_stats.html", {"stats": stats})
