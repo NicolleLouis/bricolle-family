f14ed4e8e4e31f47d62b78d9446268cda97147be
NicolleLouis <louisxnicolle@gmail.com>
2025-07-22 22:01:41 +0200

    Add purchase and sell forms for UniqueFlip

--- altered/views/unique_flip.py
+++ altered/views/unique_flip.py
@@ -1,12 +1,36 @@
-from django.shortcuts import render
 from django.db.models import Sum, F, DecimalField, ExpressionWrapper
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
 
 from altered.models import UniqueFlip
+from altered.forms import UniqueFlipPurchaseForm, UniqueFlipSellForm
 
 
 def unique_flip_list_view(request):
     status = request.GET.get('status', 'current')
-    flips = UniqueFlip.objects.all().order_by('-bought_at')
+
+    if request.method == 'POST':
+        if 'purchase' in request.POST:
+            form = UniqueFlipPurchaseForm(request.POST)
+            if form.is_valid():
+                UniqueFlip.objects.create(
+                    unique_id=form.cleaned_data['unique_id'],
+                    bought_price=form.cleaned_data['bought_price'],
+                    bought_at=timezone.now(),
+                )
+                return redirect('altered:unique_flip_list')
+        elif 'sell' in request.POST:
+            flip = get_object_or_404(UniqueFlip, id=request.POST.get('flip_id'))
+            form = UniqueFlipSellForm(request.POST)
+            if form.is_valid():
+                flip.sold_price = form.cleaned_data['sold_price']
+                flip.sold_at = timezone.now()
+                flip.save()
+                return redirect(request.path + f'?status={status}')
+    else:
+        form = UniqueFlipPurchaseForm()
+
+    flips = UniqueFlip.objects.order_by('-bought_at')
     if status == 'sold':
         flips = flips.filter(sold_at__isnull=False)
     else:
@@ -14,14 +38,18 @@ def unique_flip_list_view(request):
 
     balance = UniqueFlip.objects.aggregate(
         balance=Sum(
-            ExpressionWrapper(F('sold_price') - F('bought_price'),
-                               output_field=DecimalField(max_digits=10, decimal_places=2))
+            ExpressionWrapper(
+                F('sold_price') - F('bought_price'),
+                output_field=DecimalField(max_digits=10, decimal_places=2)
+            )
         )
-    )["balance"] or 0
+    )['balance'] or 0
 
     context = {
         'flips': flips,
         'status': status,
         'balance': balance,
+        'purchase_form': form,
+        'sell_form': UniqueFlipSellForm(),
     }
     return render(request, 'altered/unique_flip_list.html', context)
