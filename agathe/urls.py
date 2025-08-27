216e812444ee912431152ef7b100d6a84bb4e997
NicolleLouis <louisxnicolle@gmail.com>
2025-08-27 16:06:34 +0200

    feat(agathe): add home page

--- agathe/urls.py
+++ agathe/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
+from agathe.views.home import HomeController
 from agathe.views.pit_stop import PitStopController
 from agathe.views.diaper_change import DiaperChangeController
 
 app_name = "agathe"
 
 urlpatterns = [
+    path("", HomeController.home, name="home"),
     path("pit_stop/", PitStopController.pit_stop, name="pit_stop"),
     path("pit_stop/<int:pk>/finish/", PitStopController.finish, name="pit_stop_finish"),
     path("diaper_change/", DiaperChangeController.diaper_change, name="diaper_change"),
