b7136b54439d0a557b6b29305ed79e8500498a3d
Louis Nicolle <louisxnicolle@gmail.com>
2025-09-02 10:33:20 +0200

    Add sell computation

--- finance_simulator/domain/amortization_month.py
+++ finance_simulator/domain/amortization_month.py
@@ -10,9 +10,14 @@ class AmortizationMonth:
     capital_paid: float
     capital_remaining: float
     sell_cost: float | None = None
+    net_sell_cost: float | None = None
 
     def compute_sell_cost(self, simulation, total_interest):
         diagnostic_fee = SellConstants.DIAGNOSTIC
         agency_fee = float(simulation.house_cost) * SellConstants.AGENCY_FEE
         sell_cost = diagnostic_fee + agency_fee + total_interest
         self.sell_cost = sell_cost
+
+        if simulation.comparative_rent is not None:
+            rent_equivalent = (self.month + 1) * simulation.comparative_rent
+            self.net_sell_cost = float(rent_equivalent) - sell_cost
