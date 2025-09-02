543f5e7c58303df84135a7544e31f0b4c5f60fd6
Louis Nicolle <louisxnicolle@gmail.com>
2025-09-02 09:27:21 +0200

    Add default simulation

---
+++ finance_simulator/constants/default_simulation.py
@@ -0,0 +1,8 @@
+from finance_simulator.domain.simulation import Simulation
+
+DEFAULT_SIMULATION = Simulation(
+    capital=400000,
+    duration=20,
+    annual_rate=3.09,
+    comparative_rent=1600,
+)
