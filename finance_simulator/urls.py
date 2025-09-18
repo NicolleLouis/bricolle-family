8d9036bd1481e68b4cee62c3e786e40486c21c5f
NicolleLouis <louisxnicolle@gmail.com>
2025-09-18 18:11:55 +0200

    Add saved simulation selection modal

--- finance_simulator/urls.py
+++ finance_simulator/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
-from finance_simulator.views.home import default_simulation, home, save_simulation
+from finance_simulator.views.home import (
+    default_simulation,
+    home,
+    open_simulation,
+    save_simulation,
+)
 from finance_simulator.views.documents import ideas
 
 app_name = "finance_simulator"
@@ -8,6 +13,7 @@ app_name = "finance_simulator"
 urlpatterns = [
     path("", home, name="home"),
     path("default/", default_simulation, name="default_simulation"),
+    path("simulation/<int:pk>/", open_simulation, name="open_simulation"),
     path("save/", save_simulation, name="save_simulation"),
     path("ideas/", ideas, name="ideas"),
 ]
