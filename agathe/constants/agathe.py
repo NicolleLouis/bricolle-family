2ef352db0bf5d785d59fc3514555f8d067514a39
Louis Nicolle <louisxnicolle@gmail.com>
2025-09-27 12:57:45 +0200

    Update constants

--- agathe/constants/agathe.py
+++ agathe/constants/agathe.py
@@ -4,4 +4,4 @@ from datetime import date
 class AgatheConstant:
     BIRTHDATE = "2025-08-23"
     FOOD_INTERVAL = 4
-    GRAPH_START_DATE = date(2025, 8, 28)
+    GRAPH_START_DATE = date(2025, 9, 27)
