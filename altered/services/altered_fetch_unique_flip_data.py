dd8b49b5e53babd668081beb9701c1122bbf9c70
Louis Nicolle <louisxnicolle@gmail.com>
2025-10-04 17:40:08 +0200

    Improve unique image fetching

--- altered/services/altered_fetch_unique_flip_data.py
+++ altered/services/altered_fetch_unique_flip_data.py
@@ -23,14 +23,27 @@ class AlteredFetchUniqueFlipDataService:
 
     def handle(self):
         self.fetch_data()
+        print(self.data)
         self.unique.faction = self.find_faction()
         self.unique.name = self.data["name"]
-        self.unique.image_path = self.data["imagePath"]
+        self.unique.image_path = self.get_image_path()
         update_fields = ["name", "image_path", "faction"]
 
         self.unique.save(update_fields=update_fields)
 
         self.update_elements()
+
+    def get_image_path(self):
+        if "imagePath" in self.data:
+            return self.data["imagePath"]
+
+        if "allImagePath" in self.data:
+            all_image_data = self.data["allImagePath"]
+            if all_image_data['fr-fr'] is not None:
+                return all_image_data['fr-fr']
+            return all_image_data['en-us']
+        return None
+
 
     def find_faction(self):
         raw_faction_name = self.data["mainFaction"]["name"]
